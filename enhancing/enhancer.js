bf4bf67ac3c136a20b923cba4617966cf612cf51
Chance Rutledge <chancerutledge2@gmail.com>
2020-03-25 20:21:59 -0700

    basic logic and tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,18 +2,34 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
+  if(item.enhancement < 20){
+    item.enhancement = item.enhancement + 1;
+   } 
+
   return { ...item };
 }
 
 function fail(item) {
+ if(item.enhancement < 15){
+   item.durability = item.durability - 5;
+   
+ } else 
+ 
+  if(item.enhancement === 15 || item.enhancement === 16){
+    item.durability = item.durability -10;
+ } else 
+  if(item.enhancement > 16){
+   item.enhancement = item.enhancement - 1;
+ } 
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,49 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+
+
+it('repair', () => {
+    expect(enhancer.repair({
+        name: 'Chungus',
+        durability:  85,
+        enhancement: 20
+    })).toEqual({
+        name: 'Chungus',
+        durability:  100,
+        enhancement: 20
+    });
+})
+
+it('succeed', () => {
+    expect(enhancer.succeed({
+        name: 'Chungus',
+        durability:  100,
+        enhancement: 10
+    }))
+    .toEqual({
+        name: 'Chungus',
+        durability:  100,
+        enhancement: 11
+    });
+    
+
+})
+
+
+it('fail', () => {
+    expect(enhancer.fail({
+        name: 'Chungus',
+        durability:  15,
+        enhancement: 17
+    }))
+    .toEqual({
+        name: 'Chungus',
+        durability:  15,
+        enhancement: 16
+    });
+    
+
+})
+
